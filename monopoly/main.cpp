f01540263c1609bbd3c472467df134e59205fd03
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2025-10-12 14:37:25 +0300

    Initial Commit

---
+++ monopoly/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  monopoly
+//
+//  Created by Ibrahim Habib on 12/10/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
